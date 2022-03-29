84461f3f2e71c8c08a86674ca6eb35f1a957cd83
Vidhi Kapadia <vidhikap27@gmail.com>
2022-03-29 18:23:24 +0530

    Added Practical02

---
+++ practical02/src/components/CardData.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <img
+      :src="carData.image"
+      class="card-img-top a_card_img"
+      alt="Audi E-tron"
+    />
+    <div class="card-body main-card">
+      <h5 class="card-title">{{ carData.heading }}</h5>
+      <p class="card-text cardHeight">
+        {{ carData.details }}
+      </p>
+    </div>
+    <div class="text-center">
+      <button
+        class="btn btn-primary"
+        v-if="
+          carData.price === ''
+            ? (this.buttontext = 'Available Soon')
+            : (this.buttontext = 'Info')
+        "
+        :disabled="carData.price === ''"
+        @click="carPrice(carData.price)"
+      >
+        {{ this.buttontext }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    carData: Object,
+  },
+  data() {
+    return {
+      buttontext: "Info",
+    };
+  },
+  methods: {
+    carPrice(priceparams) {
+      console.log(priceparams);
+      this.$emit("price", priceparams);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.demo {
+  margin-top: 20px;
+  width: 18rem;
+  height: 25rem;
+}
+.a_card_img {
+  width: 100%;
+  height: 160px;
+}
+.cardHeight {
+  height: 8.3rem;
+}
+</style>
---
+++ practical02/src/components/NavBarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-success">
+      <div class="container-fluid center">
+        <span class="navbar-brand mb-0 h1 mx-auto">Car Showroom</span>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
