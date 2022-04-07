9d5aff9838219b0593b6a6a32c0d2982a6d53250
Vidhi Kapadia <vidhikap27@gmail.com>
2022-04-07 11:19:03 +0530

    committed add car details,delete car

---
+++ practical03/src/components/NavBarComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-success">
+      <div class="container-fluid center">
+        <span class="navbar-brand mb-0 h1">Car Showroom</span>
+        <button
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#staticBackdrop"
+          class="btn btn-primary"
+        >
+          Add Car
+        </button>
+      </div>
+
+      <ModalComponent />
+    </nav>
+  </div>
+</template>
+
+<script>
+import ModalComponent from "./ModalComponent.vue";
+export default {
+  components: {
+    ModalComponent,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
