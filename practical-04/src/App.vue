873b190706ba47f06acfc3ff85d7df138652b206
Vidhi Kapadia <vidhikap27@gmail.com>
2022-04-25 14:23:58 +0530

    added api integration

---
+++ practical-04/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <app-header />
+    <HomePage />
+  </div>
+</template>
+
+<script>
+import HomePage from "./components/HomePage.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
