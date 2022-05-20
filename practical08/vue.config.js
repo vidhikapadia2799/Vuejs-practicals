3f180c2b698da9af4a5ddcaff7b08417d40e5f67
Vidhi Kapadia <vidhikap27@gmail.com>
2022-05-20 11:42:11 +0530

    deleted vue.config file

--- practical08/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    proxy:
-      "http://www.mockbin.org/bin/cfbeadb5-3269-49af-ade2-515996af364c?foo=bar&foo=baz",
-  },
-});
